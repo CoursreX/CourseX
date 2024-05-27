0226098da21589b25412bc1a12bd91ea0640fe48
haesa <xdhaesa@gmail.com>
2024-05-28 01:10:53 +0900

    #7 [REFACTOR] 사이드바 메뉴 unactive 로직 함수로 분리

--- src/main/webapp/resources/js/updateSelectedMenu.js
+++ src/main/webapp/resources/js/updateSelectedMenu.js
@@ -1,13 +1,19 @@
+function unActiveMenu() {
+    const menus = document.querySelectorAll('.sidebar__menu');
+
+    menus.forEach((menu) => {
+        const img = menu.querySelector('img');
+        img.src = img.getAttribute('data-original-src');
+        menu.classList.remove('selected');
+    });
+}
+
 function updateSelectedMenu() {
     const menus = document.querySelectorAll('.sidebar__menu');
 
     menus.forEach(menu => {
         menu.addEventListener('click', (event) => {
-            menus.forEach((m) => {
-                const img = m.querySelector('img');
-                img.src = img.getAttribute('data-original-src');
-                m.classList.remove('selected');
-            });
+            unActiveMenu(menus);
 
             // 클릭된 메뉴에 'selected' 클래스를 추가하고 src를 변경
             const selectedImg = event.currentTarget.querySelector('img');
