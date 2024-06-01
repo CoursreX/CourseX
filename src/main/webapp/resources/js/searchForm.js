e3ee23fb2346001b1b371497656a71eb48174e09
ysy7838 <ysy7838@gmail.com>
2024-06-02 01:29:28 +0900

    메인페이지 수정중 수강신청 페이지-라디오버튼마다 검색칸 뜨지않는문제발생

---
+++ src/main/webapp/resources/js/searchForm.js
@@ -0,0 +1,14 @@
+// 라디오 버튼 선택에 따라 검색 입력칸 표시/숨김 처리
+function handleRadio(event) {
+    const searchFilterElements = document.querySelectorAll('.search__filter__element');
+
+    searchFilterElements.forEach(element => element.style.display = 'none');
+
+    if(event.target.value === 'code') {
+        searchFilterElements[0].style.display = 'block';
+    } else if(event.target.value === 'name') {
+        searchFilterElements[1].style.display = 'block';
+    } else if(event.target.value === 'category') {
+        searchFilterElements[2].style.display = 'block';
+    }
+}
